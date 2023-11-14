fda071533f252d62a174f4bd1301043e7d990dbe
rushilbh <ytgamingrb@gmail.com>
2023-11-14 06:37:31 +0530

    initial commit

---
+++ src/main/java/com/sb/learning/bookmyshow/controller/SearchController.java
@@ -0,0 +1,12 @@
+package com.sb.learning.bookmyshow.controller;
+
+import com.sb.learning.bookmyshow.model.EventDTO;
+import com.sb.learning.bookmyshow.model.SearchCriteriaDTO;
+
+import java.util.List;
+
+public class SearchController {
+    public List<EventDTO> search(SearchCriteriaDTO searchCriteria){
+
+    }
+}
